a67cffede7c77302102a95813f4e2e44610cba11
sithija jayasinghe <harshanajayasinghe113@gmail.com>
2026-04-16 13:29:58 +0530

    add staff entity: create Staff class with JPA annotations and relationships

--- src/main/java/org/edu/entity/Staff.java
+++ src/main/java/org/edu/entity/Staff.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "teachers")
+@Table(name = "staff")
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
